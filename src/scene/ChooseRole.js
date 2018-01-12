31bf9375efa446e7d0a3bcd5c11c55b339879780
likenan <15991623813@163.com>
2018-01-12 18:57:33 +0800

    角色界面修改

--- src/scene/ChooseRole.js
+++ src/scene/ChooseRole.js
@@ -62,6 +62,15 @@ var ChooseRoleUILayer = cc.Layer.extend({
         this._super();
 
         this.nameLabel = new cc.Sprite('#Game name.png');
+        this.role1 = new cc.Sprite('#Role1.png');
+        this.role2 = new cc.Sprite('#Role2.png');
+        this.role3 = new cc.Sprite('#Role3.png');
+        this.role4 = new cc.Sprite('#Role4.png');
+        this.role5 = new cc.Sprite('#Role5.png');
+        this.unlock = new cc.Sprite('#unlock.png');
+        this.unlock1 = new cc.Sprite('#unlock.png');
+        this.selected = new cc.Sprite('#Role pitch on.png');
+        this.three = new cc.Sprite('#Three kilometers.png');
 
         this.backBtn = new Button(
             'Get back_UI.png', 'Get back_UI_Hit.png',
@@ -72,7 +81,16 @@ var ChooseRoleUILayer = cc.Layer.extend({
 
                 cc.director.runScene(homeSene)
             }
-        )
+        );
+
+        this.canUnlock = new Button(
+            'Can unlock_UI.png', 'Can unlock_UI_Hit.png',
+            function () {
+
+            }, function () {
+                cc.log('可解锁')
+            }
+        );
     },
 
     init: function () {
@@ -81,6 +99,46 @@ var ChooseRoleUILayer = cc.Layer.extend({
         this.backBtn.setAnchorPoint(1, 0);
         this.backBtn.setPosition(size.width - 1800, size.height - 100);
         this.addChild(this.backBtn, 0);
+
+        this.role5.setAnchorPoint(1, 0);
+        this.role5.setPosition(size.width - 20, size.height - 900);
+        this.addChild(this.role5, 0);
+
+        this.role2.setAnchorPoint(1, 0);
+        this.role2.setPosition(size.width - 400, size.height - 900);
+        this.addChild(this.role2, 0);
+
+        this.role1.setAnchorPoint(1, 0);
+        this.role1.setPosition(size.width - 800, size.height - 850);
+        this.addChild(this.role1, 0);
+
+        this.role3.setAnchorPoint(1, 0);
+        this.role3.setPosition(size.width - 1200, size.height - 900);
+        this.addChild(this.role3, 0);
+
+        this.role4.setAnchorPoint(1, 0);
+        this.role4.setPosition(size.width - 1600, size.height - 900);
+        this.addChild(this.role4, 0);
+
+        this.unlock.setAnchorPoint(1, 0);
+        this.unlock.setPosition(size.width - 1650, size.height - 1000);
+        this.addChild(this.unlock, 0);
+
+        this.unlock1.setAnchorPoint(1, 0);
+        this.unlock1.setPosition(size.width - 1250, size.height - 1000);
+        this.addChild(this.unlock1, 0);
+
+        this.selected.setAnchorPoint(1, 0);
+        this.selected.setPosition(size.width - 860, size.height - 1000);
+        this.addChild(this.selected, 0);
+
+        this.canUnlock.setAnchorPoint(1, 0);
+        this.canUnlock.setPosition(size.width - 420, size.height - 1050);
+        this.addChild(this.canUnlock, 0);
+
+        this.three.setAnchorPoint(1, 0);
+        this.three.setPosition(size.width - 20, size.height - 1000);
+        this.addChild(this.three, 0);
 
     },
 
